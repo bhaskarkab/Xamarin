87431b460d6ba6213c0c5b222f7104d0fa4fb77e
bhaskar katkam <bhaskarkatkam@bhaskars-MacBook-Air.local>
2018-08-17 18:53:10 +0530

    Initial Upload

---
+++ HelloWorld/GreetPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HelloWorld
+{
+    public partial class GreetPage : ContentPage
+    {
+        public GreetPage()
+        {
+            InitializeComponent();
+
+            slider.Value = 0.5;
+        }
+
+        //void Handle_ValueChanged(object sender, Xamarin.Forms.ValueChangedEventArgs e)
+        //{
+        //    lblTitle.Text = String.Format("Value is {0:F2}", e.NewValue);
+        //}
+    }
+}
